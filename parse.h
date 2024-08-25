c7642065169893f3374765b8dd9610ff53e5af94
Avani Rai <120315095+avani-rai@users.noreply.github.com>
2024-08-26 02:51:32 +0530

    Update parse.h
    parse file modified

--- parse.h
+++ parse.h
@@ -8,74 +8,78 @@ using namespace std;
 /** This file handles parsing the data we are going to work with **/
 /** It also holds all the global variables we parse into         **/
 
-
+// Global variables
 string operation;
-int last_instant, process_count;
+int last_instant;
+int process_count;
 vector<pair<char, int>> algorithms;
-vector<tuple<string,int,int>> processes;
-vector<vector<char>>timeline;
-unordered_map<string,int>processToIndex;
+vector<tuple<string, int, int>> processes;
+vector<vector<char>> timeline;
+unordered_map<string, int> processToIndex;
 
+// Results
+vector<int> finishTime;
+vector<int> turnAroundTime;
+vector<float> normTurn;
 
-//Results
-
-vector<int>finishTime;
-vector<int>turnAroundTime;
-vector<float>normTurn;
-
-
-void parse_algorithms(string algorithm_chunk)
+// Function to parse the algorithms from the input string
+void parse_algorithms(const string& algorithm_chunk)
 {
     stringstream stream(algorithm_chunk);
-    while (stream.good())
+    string temp_str;
+
+    while (getline(stream, temp_str, ','))
     {
-        string temp_str;
-        getline(stream, temp_str, ',');
         stringstream ss(temp_str);
-        getline(ss, temp_str, '-');
-        char algorithm_id = temp_str[0];
-        getline(ss, temp_str, '-');
-        int quantum = temp_str.size() >= 1 ? stoi(temp_str) : -1;
-        algorithms.push_back( make_pair(algorithm_id, quantum) );
+        char algorithm_id;
+        int quantum = -1;
+
+        ss >> algorithm_id;
+        if (ss.peek() == '-') 
+        {
+            ss.ignore(); // ignore the '-'
+            ss >> quantum;
+        }
+
+        algorithms.emplace_back(algorithm_id, quantum);
     }
 }
 
+// Function to parse the processes
 void parse_processes()
 {
-    string process_chunk, process_name;
-    int process_arrival_time, process_service_time;
-    for(int i=0; i<process_count; i++)
+    for (int i = 0; i < process_count; ++i)
     {
+        string process_chunk;
         cin >> process_chunk;
 
         stringstream stream(process_chunk);
-        string temp_str;
-        getline(stream, temp_str, ',');
-        process_name = temp_str;
-        getline(stream, temp_str, ',');
-        process_arrival_time = stoi(temp_str);
-        getline(stream, temp_str, ',');
-        process_service_time = stoi(temp_str);
+        string process_name;
+        int process_arrival_time, process_service_time;
 
-        processes.push_back( make_tuple(process_name, process_arrival_time, process_service_time) );
+        getline(stream, process_name, ',');
+        stream >> process_arrival_time;
+        stream.ignore(); // ignore the ','
+        stream >> process_service_time;
+
+        processes.emplace_back(process_name, process_arrival_time, process_service_time);
         processToIndex[process_name] = i;
     }
 }
 
+// Function to parse the overall input
 void parse()
 {
     string algorithm_chunk;
     cin >> operation >> algorithm_chunk >> last_instant >> process_count;
+
     parse_algorithms(algorithm_chunk);
     parse_processes();
+
     finishTime.resize(process_count);
     turnAroundTime.resize(process_count);
     normTurn.resize(process_count);
-    timeline.resize(last_instant);
-    for(int i=0; i<last_instant; i++)
-        for(int j=0; j<process_count; j++)
-            timeline[i].push_back(' ');
+    timeline.assign(last_instant, vector<char>(process_count, ' '));
 }
-
 
 #endif // PARSER_H_INCLUDED
